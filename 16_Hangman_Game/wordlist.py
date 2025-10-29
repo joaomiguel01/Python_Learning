17dc94fc4b24e057977542c8ae4023601ee8a398
João Miguel <jmocarvallho7123@gmail.com>
2025-10-29 12:35:09 -0300

    Add new Project

---
+++ 16_Hangman_Game/wordlist.py
@@ -0,0 +1,9 @@
+# Words for hangman game
+
+words = (
+    "leao", "tigre", "elefante", "girafa", "zebra", "cavalo", "macaco", "gorila",
+    "urso", "lobo", "raposa", "cachorro", "gato", "leopardo", "chita", "rinoceronte",
+    "hipopotamo", "crocodilo", "jacare", "tartaruga", "cobra", "sapo", "peixe", "golfinho",
+    "baleia", "tubarao", "pinguim", "aguia", "coruja", "papagaio", "pato", "galinha",
+    "galo", "vaca", "boi", "ovelha", "cabra", "porco", "camelo", "canguru"
+)
