9cd5de57c75e2670df82297fad78e8637a8ba795
João Miguel <jmocarvallho7123@gmail.com>
2025-12-05 09:16:42 -0300

    1st Project

---
+++ 1_MadLibs_Game/app.py
@@ -0,0 +1,12 @@
+# MadLibs Python Game
+
+adj1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adj2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending 'ing': ")
+adj3 = input("Enter an adjective (description): ")
+
+print(f"\nToday I went to a {adj1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adj2} and {verb1}")
+print(f"I was {adj3}!")
