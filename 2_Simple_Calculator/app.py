9aac517a8d44b5fd4e224d19c418774134205ddb
João Miguel <jmocarvallho7123@gmail.com>
2025-12-06 10:12:38 -0300

    2nd Project

---
+++ 2_Simple_Calculator/app.py
@@ -0,0 +1,21 @@
+# Python Calculator
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1+num2
+    print(f"{result:.3f}")
+elif operator == "-":
+    result = num1-num2
+    print(f"{result:.3f}")
+elif operator == "*":
+    result = num1*num2
+    print(f"{result:.3f}")
+elif operator == "/":
+    result = num1/num2
+    print(f"{result:.3f}")
+else:
+    print(f"{operator} is not a valid operator!")
+
