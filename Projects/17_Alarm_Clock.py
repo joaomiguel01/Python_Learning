50369b170630d92bf563396074fa8fc6f89c4b66
João Miguel <jmocarvallho7123@gmail.com>
2025-12-30 20:45:40 -0300

    Add Project

---
+++ Projects/17_Alarm_Clock.py
@@ -0,0 +1,26 @@
+# Python Alarm Clock
+import datetime
+from time import sleep
+from soundplay import playsound
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = '17_Project_Sound.mp3'
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("WAKE UP! 🤯!")
+
+            playsound(sound_file)
+
+            is_running = False
+
+        sleep(1)
+
+if __name__ == "__main__":
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
