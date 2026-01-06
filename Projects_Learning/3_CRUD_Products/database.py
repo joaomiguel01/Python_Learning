f5bceec5a554290415ccbf06f27d68ec361fae30
João Miguel <jmocarvallho7123@gmail.com>
2026-01-06 11:25:31 -0300

    Finishing 3º Project

--- Projects_Learning/3_CRUD_Products/database.py
+++ Projects_Learning/3_CRUD_Products/database.py
@@ -18,6 +18,9 @@ class Product:
     @code.setter
     def code(self, code: str):
         if isinstance(code, str) and code.strip():
+            if not code.isalnum():
+                raise ValueError("ERROR: Code must be alphanumeric!")
+            
             if len(code) == 6:
                 self._code = code.strip()
             else:
@@ -66,13 +69,78 @@ class Product:
 
 
 
+    def to_dict(self):
+        return {
+            'code': self.code,
+            'name': self.name,
+            'price': self.price,
+            'qtd': self.qtd
+        }
+    
+
+
+
+    @staticmethod
+    def from_dict(data: dict):
+        return Product(**data)
+
+
 
     def __str__(self):
-        return f"{self.code:^6} | {self.name:<25} | R${self.price:<7.2f} | {self.qtd:<7}"
+        return f"{self.code:^6} | {self.name:<25} | {self.price:<9.2f} | {self.qtd:<7}"
 
 
 # Database functions
+def search_product(data: list[dict], code: str):
+    products = [Product.from_dict(d) for d in data]
 
+    for index, prod in enumerate(products):
+        if prod.code == code:
+            return index
+        
+    return -1
+
+def add_product(arq: str, prod: Product):
+    data = grab_data(arq)
+
+    if search_product(data, prod.code) != -1:
+        raise ValueError("ERROR: This code's product exists in system!")
+
+    data.append(prod.to_dict())
+    save_data(arq, data)
+
+
+def delete_product(arq: str, prod_cod: str):
+    data = grab_data(arq)
+    index_prod = search_product(data, prod_cod)
+
+    if index_prod != -1:
+        data.pop(index_prod)
+        save_data(arq, data)
+    else:
+        raise ValueError("ERROR: This product doesn't exist!")
+
+
+def update_product(arq: str, prod_cod: str, option: int, new_value):
+    data = grab_data(arq)
+    index_prod = search_product(data, prod_cod)
+
+    if index_prod != -1:
+        prod = Product.from_dict(data[index_prod])
+        match option:
+            case 1:
+                
+                prod.code = new_value
+            case 2:
+                prod.name = new_value
+            case 3:
+                prod.price = new_value
+            case 4:
+                prod.qtd = new_value
+        data[index_prod] = prod.to_dict()
+        save_data(arq, data)
+    else:
+        raise ValueError("ERROR: This product doesn't exist!")
 
 # Data Processing functions
 def ensure_exists_file(arq: str):
@@ -81,4 +149,28 @@ def ensure_exists_file(arq: str):
         sleep(1)
         with open(arq, 'x', encoding="utf-8") as file:
             json.dump([], file, indent=4, ensure_ascii=False)
-     
+
+
+def grab_data(arq: str):
+    try:
+        with open(arq, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        return []
+
+
+def save_data(arq: str, data: list[dict]):
+    with open(arq, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def print_data(arq: str):
+    data = grab_data(arq)
+    products = [Product.from_dict(d) for d in data]
+
+    title("LISTA DE PRODUTOS CADASTRADOS")
+    print(f"{'CÓDIGO':^6} | {'NOME':<25} | {'PREÇO(R$)':<9} | {'QTD':<7}")
+    print('-'*57)
+
+    for prod in products:
+        print(prod)
--- Projects_Learning/3_CRUD_Products/main.py
+++ Projects_Learning/3_CRUD_Products/main.py
@@ -1,10 +1,10 @@
-from gui_utils import title, menu, read_option
+from gui_utils import title, menu, read_option, read_float, read_int, read_name
 from time import sleep
-from database import ensure_exists_file
+from database import ensure_exists_file, add_product, Product, print_data, delete_product, grab_data, update_product
 
 def main():
-
-    ensure_exists_file("database.json")
+    arq = "database.json"
+    ensure_exists_file(arq)
     title("ARQUIVO CARREGADO COM SUCESSO!")
     sleep(1)
 
@@ -19,27 +19,66 @@ def main():
             print()
             choice = read_option("Digite a opção desejada: ", 5)
             sleep(1)
+            print()
 
             match choice:
                 case 1:
                     # Register Product
-                    pass
+                    title("NOVO PRODUTO A SER CADASTRADO")
+                    code = read_name("Código do Produto (6 caracteres): ")
+                    name = read_name("Nome do Produto: ")
+                    price = read_float("Preço do Produto: R$")
+                    qtd = read_int("Quantidade no estoque: ")
+                    add_product(arq, Product(code, name, price, qtd))
+                    sleep(1)
+                    print()
+                    title("PRODUTO CADASTRADO COM SUCESSO!")
+                    sleep(1)
                 case 2:
                     # List Products
-                    pass
+                    print_data(arq)
                 case 3:
                     # Delete Product
-                    pass
+                    if grab_data(arq):
+                        title("PROCEDIMENTO DE DELEÇÃO")
+                        sleep(1)
+                        print_data(arq)
+                        code_deleter = read_name("\nDigite o código do produto a ser deletado: ")
+                        delete_product(arq, code_deleter)
+                        title("PRODUTO DELETADO COM SUCESSO!")
+                    else:
+                        raise ValueError("ERROR: The database is empty!")
                 case 4:
                     # Update Product
-                    pass
+                    if grab_data(arq):
+                        title("PROCEDIMENTO DE ATUALIZAÇÃO")
+                        sleep(1)
+                        print_data(arq)
+                        code_updater = read_name("\nDigite o código do produto a ser deletado: ")
+                        menu("PROPRIEDADES A SEREM ATUALIZADAS", ["Codígo do Produto",
+                                                                  "Nome do Produto",
+                                                                  "Preço do Produto",
+                                                                  "Quantidade"])
+                        op = read_option("Escolha a propriedade desejada: ", 4)
+                        match op:
+                            case 1:
+                                new_value = read_name("\nDigite o novo código do produto (6 caracteres): ")
+                            case 2:
+                                new_value = read_name("\nDigite o novo nome do produto: ")
+                            case 3:
+                                new_value = read_float("\nDigite o novo preço do produto: R$")
+                            case 4:
+                                new_value = read_int("\nDigite a nova quantidade do produto: ")
+                        update_product(arq, code_updater, op, new_value)
+                        title("PRODUTO ATUALIZADO COM SUCESSO!")
+                        sleep(1)
                 case 5:
                     # Exit System
                     title("SAINDO DO SISTEMA...")
                     sleep(1)
                     title("ATÉ LOGO!")
                     break
-
+            sleep(1)
         except Exception as e:
             title("HOUVE UM ERRO AO REALIZAR O PROCEDIMENTO")
             print(e)
